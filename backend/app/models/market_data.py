2d750f7ddaaf84dae72db9db062e083ef33ce6e9
sasan333333 <sasanrasoolzadeh.sr@gmail.com>
2026-09-11 03:14:36 +0330

    feat: add TSETMC data normalization

--- backend/app/models/market_data.py
+++ backend/app/models/market_data.py
@@ -8,3 +8,17 @@ class MarketData:
     last_price: Optional[float] = None
     close_price: Optional[float] = None
     volume: Optional[int] = None
+
+    @property
+    def price_change(self) -> Optional[float]:
+        if self.last_price is None or self.close_price is None:
+            return None
+
+        return self.last_price - self.close_price
+
+    @property
+    def price_change_percent(self) -> Optional[float]:
+        if self.close_price in (None, 0) or self.last_price is None:
+            return None
+
+        return ((self.last_price - self.close_price) / self.close_price) * 100
