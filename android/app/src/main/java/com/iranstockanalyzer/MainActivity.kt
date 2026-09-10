b5574506bdbe36b59eb351fb1dc294fb16959fce
sasan333333 <sasanrasoolzadeh.sr@gmail.com>
2026-09-11 02:41:32 +0330

    feat: add TSETMC data normalization

--- android/app/src/main/java/com/iranstockanalyzer/MainActivity.kt
+++ android/app/src/main/java/com/iranstockanalyzer/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.iranstockanalyzer
 
-import android.app.Activity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.iranstockanalyzer.data.api.RetrofitClient
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val textView = TextView(this)
+        textView.text = "Loading Market Data..."
+        setContentView(textView)
+
+        CoroutineScope(Dispatchers.Main).launch {
+            try {
+                val data = RetrofitClient.apiService.marketData()
+                textView.text = data.toString()
+            } catch (e: Exception) {
+                textView.text = "Error: ${e.message}"
+            }
+        }
     }
 }
