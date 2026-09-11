6cf2f8ad310a9096c596c17e5c08e55de9824fb1
sasan333333 <sasanrasoolzadeh.sr@gmail.com>
2026-09-11 04:06:36 +0330

    03-V-1: Add Telegram Service

---
+++ backend/app/services/telegram_service.py
@@ -0,0 +1,25 @@
+import requests
+
+
+class TelegramService:
+
+    BASE_URL = "https://api.telegram.org"
+
+    def __init__(self, bot_token: str):
+        self.bot_token = bot_token
+
+    def send_message(self, chat_id: str, text: str):
+        url = f"{self.BASE_URL}/bot{self.bot_token}/sendMessage"
+
+        response = requests.post(
+            url,
+            data={
+                "chat_id": chat_id,
+                "text": text,
+            },
+            timeout=20,
+        )
+
+        response.raise_for_status()
+
+        return response.json()
