aad2251c3343624f325e66314d781302508b97a4
sasan333333 <sasanrasoolzadeh.sr@gmail.com>
2026-09-11 03:30:18 +0330

    feat: add TSETMC data normalization
    Added new dependencies for Kotlin Coroutines and MPAndroidChart.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,8 +17,15 @@ android {
 }
 
 dependencies {
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
+
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
+
     testImplementation("junit:junit:4.13.2")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.9.0")
 }
